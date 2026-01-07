403062e6666fb173f72d1f1dddb9261087e87725
Rizaldi87 <rizaldiibnum@gmail.com>
2026-01-07 12:42:25 +0700

    fix (create courses dto) : fix status type

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,20 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { COURSESTATUS } from '@prisma/client';
 import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateCourseDto {
+  @ApiProperty({ example: 'Learning English' })
   @IsNotEmpty()
   @IsString()
   title: string;
 
+  @ApiProperty({ example: 'Learn English is good' })
   @IsOptional()
   @IsString()
   description?: string;
 
+  @ApiProperty({ example: 'https://example.com/image.jpg' })
   @IsOptional()
   @IsString()
   image?: string;
 
+  @ApiProperty({ example: 'published' })
   @IsOptional()
   @IsEnum(COURSESTATUS)
-  status?: string;
+  status?: COURSESTATUS;
 }
