a19988496577dea9ca6453fb743ce5b903f50b77
Rizaldi87 <rizaldiibnum@gmail.com>
2026-01-10 00:12:38 +0700

    add refreshtoken table

---
+++ prisma/migrations/20260109011534_add_refresh_tokent_model/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "RefreshToken" (
+    "id" SERIAL NOT NULL,
+    "token" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "revoked" BOOLEAN NOT NULL DEFAULT false,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "RefreshToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RefreshToken_token_key" ON "RefreshToken"("token");
