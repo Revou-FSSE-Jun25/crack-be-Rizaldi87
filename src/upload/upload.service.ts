6972085dc28c0dbc400dce0d322573fad99382aa
Rizaldi87 <rizaldiibnum@gmail.com>
2026-01-13 19:36:43 +0700

    (deprecated) handel file uploading

---
+++ src/upload/upload.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UploadService {
+  //   uploadImage(file: Express.Multer.File) {
+  //     return {
+  //       message: 'File uploaded successfully',
+  //       filename: file.filename,
+  //       originalName: file.originalname,
+  //       size: file.size,
+  //       path: file.path,
+  //       url: `/uploads/${file.filename}`,
+  //     };
+  //   }
+}
